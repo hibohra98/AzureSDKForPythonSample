f9d28c88472ab5f4dfcd3cfd46668a97b4819a85
Himanshu Bohra <hibohra@microsoft.com>
2022-01-12 16:15:27 +0530

    Update accessSecretKV.py

--- accessSecretKV.py
+++ accessSecretKV.py
@@ -4,8 +4,8 @@ from azure.keyvault.secrets import SecretClient
 d_credentials = AzureCliCredential()
 
 secret_client = SecretClient(
-    vault_url="https://testingkvcx.vault.azure.net/", credential=d_credentials
+    vault_url="https://<vaultName>.vault.azure.net/", credential=d_credentials
 )
-my_secret = secret_client.get_secret(name="testsecretval")
+my_secret = secret_client.get_secret(name="<secret name>")
 
 print(my_secret.value)
